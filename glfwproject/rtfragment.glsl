20ad05ad3efc32943befb233c9462fe6921e8420
Maxwell Stalsitz <maxstalsitz@gmail.com>
2022-12-29 12:29:53 -0500

    improved ray tracing lighting & reflections

--- glfwproject/rtfragment.glsl
+++ glfwproject/rtfragment.glsl
@@ -104,10 +104,11 @@ float occlusion( in vec3 pos, in vec3 nor )
     return res;					  
 }
 
-vec3 lig = normalize( vec3( -0.8, 0.3, -0.5 ) );
+vec3 lig = vec3( 0.0, 2.0, 0.0);
 
-vec3 shade( in vec3 rd, in vec3 pos, in vec3 nor, in float id, in vec3 uvw, in float dis, vec2 uv, vec3 mate)
+vec3 shade( in vec3 rd, in vec3 pos, in vec3 nor, in float id, in vec3 uvw, float dis, vec2 uv, vec3 mate)
 {
+    /*
     vec3 finalColor;
     if (useLighting){
         vec3 light = normalize(lig - pos);
@@ -148,19 +149,72 @@ vec3 shade( in vec3 rd, in vec3 pos, in vec3 nor, in float id, in vec3 uvw, in f
         vec3 col = mate * ((bac + ambient + fre) * occ + (vec3(pow(diffuse, 0.4545)) + (specular * occ)) * sha); //+refval
 
         float r = clamp(qe.x,0.0,1.0);
-
+        
         if (fogEnabled){
             float fog = smoothstep(4.0, 50.0, dis) * fogVisibility;
 	        col = mix(col, background, fog);
         }
 
-
         finalColor = col;
+
     }
     else
         finalColor = mate;
 
     return finalColor;
+    */
+
+    vec3 col;
+	
+	vec3 normal = nor;
+
+	vec3 lp;
+	lp = lig;
+
+	vec3 light = normalize(lp - pos);
+	vec3 H = reflect(-light, normal);
+	vec3 V = -rd;
+
+	vec3 specularColor = vec3(0.5);
+	float specularPower = 10.0;
+	vec3 specular = specularColor * pow(clamp(dot(H, V), 0.0, 1.0), specularPower);
+	vec3 material = mate;
+
+	float diffuse;
+	diffuse = clamp(dot(normal, normalize(lp - pos)), 0.0, 1.0);
+	diffuse *= 5.0 / dot(light - pos, light - pos);
+	vec3 ambient = material * 0.25;
+
+	float fresnel = 0.25 * pow(1.0 + dot(rd, normal), 3.0);
+
+	float occ;
+
+    float dif = clamp( dot(nor,lig), 0.0, 1.0 );
+    float sha = 1.0;
+        if( dif>0.001 ) sha = shadow( pos, lig );
+
+	if (ambientOcclusion){
+            occ = occlusion( pos, nor );
+            occ = occ*0.5 + 0.5*occ*occ;
+    }
+    else
+        occ = 1;
+
+	vec3 back = vec3(1) * 0.05 * clamp(dot(normal, -light), 0.0, 1.0);
+
+	//float d = softshadow(pos + normal * 0.025, light, 0.01, MAX_STEPS, 14);
+	float d = 1;
+
+	float diff = max(dot(normal, light), 0.);
+
+	col = material * ((back + ambient + fresnel) * occ + (vec3(pow(diffuse, 0.4545)) + (specular * occ)) * sha);
+
+	/*if (fogEnabled){
+		float fog = smoothstep(4.0, falloff, t.x) * fogVisibility;
+		col = mix(col, background, fog);
+	}*/
+
+	return col;	
 }  
 
 float rand(vec2 co){
@@ -188,7 +242,7 @@ vec3 trace( in vec3 ro, in vec3 rd, vec3 col, in float tmin, vec2 uv)
 	    {
 		    vec4 sph = sphere[i];
 	        float h = sphIntersect( ro, rd, sph); 
-		    if( h>0.0 && h<t ) 
+		    if( h>0.0 && h<t ) //t = min distance
 		    {
 			    t = h;
                 obj = sph;
@@ -198,43 +252,70 @@ vec3 trace( in vec3 ro, in vec3 rd, vec3 col, in float tmin, vec2 uv)
 
         vec3 nor, pos;
 
+        //shade reflection as whole sphere instead of half sphere
+
         if( id != -1.0)
         {
 		    pos = ro + t*rd;
 		    nor = sphNormal( pos, obj );
 
-            passCol = shade( rd, pos, nor, id, pos-obj.xyz, 0.01, uv, vec3(0.5)); 
+            passCol = shade( rd, pos, nor, id, pos-obj.xyz, t, uv, vec3(0.61, 0.176, 0.176)); 
         }
         else{
+            //distance to plane
             float t = (-1.0-ro.y)/rd.y; //-1.0 = height
-            if( t>0.0 )
+            if( t > -1.0)
             {
                 tmin = t;
-                vec3 pos = ro + t*rd;
-                vec3 nor = vec3(0.0,1.0,0.0); // plane
-                vec3 tile = mix(vec3(0.0 + 1.0 * mod(floor(pos.x) + floor(pos.z), 2.0)), vec3(0.773, 0.725, 0.627), 0.5); //from inigo quilez
+                pos = ro + t*rd;
+                nor = vec3(0.0,1.0,0.0); // plane
+                vec3 tile = mix(vec3(0.0 + 1.0 * mod(floor(pos.x) + floor(pos.z), 2.0)), vec3(0.773, 0.725, 0.627), 0.5); //from inigo quilez (with modified colors)
 
-                if (i == 0)
-                    return shade( rd, pos, nor, 0.0, pos*0.5, t, uv, tile);
-                else
-                    passCol = shade( rd, pos, nor, 0.0, pos*0.5, t, uv, tile);
+                if (i == 0){
+                    rd = reflect (rd, nor);
+                    ro = pos;
+
+                    t = tmin;
+                    id  = -1.0;
+                    obj = vec4(0.0);
+
+                    for( int i=0; i<NUMSPHEREES; i++ )
+	                {
+		                vec4 sph = sphere[i];
+	                    float h = sphIntersect( ro, rd, sph); 
+		                if( h>0.0 && h<t ) //t = min distance
+		                {
+			                t = h;
+                            obj = sph;
+			                id = float(i);	
+                        }
+	                }
+
+                    if (id == -1.0){
+				        return shade( rd, pos, nor, id, pos, t, uv, tile);
+                        //passCol = shade( rd, pos, nor, id, pos, t, uv, tile);
+                    }
+                }
+
+                passCol = shade( rd, pos, nor, id, pos, t, uv, tile);
             }
             else{
                 if (i == 0)
                     return background;
-                else
-                    passCol = background;
+					
+                passCol = background;
 			}
                 
         }
 
-        if (i == 0){
+        if (i == 0)
             col = passCol;
-            rd = reflect (rd, nor);
-            ro = pos;
-        }
         else
-            col += mix(col, passCol, 0.5);
+		    col += mix(col, passCol, 0.5);
+        
+        
+        rd = reflect (rd, nor);
+        ro = pos;
     }
 
     return col;
@@ -249,31 +330,43 @@ mat3 getCam(vec3 rayOrigin)
 
 vec3 render(vec2 uv)
 {
+
 	vec2 q = gl_FragCoord.xy / resolution.xy;
 
 	float time = time*0.5;
 
 
-    for (int i = 0; i < NUMSPHEREES; i++){
+    /*for (int i = 0; i < NUMSPHEREES; i++){
         float ra = pow(2/float(NUMSPHEREES-1),4.0);
         ra = 0.2 + 0.8*ra;
         ra *= 1.0; // scale
 
         float x = float(i % 10);
 		float z = float(i / 10);
-	sphere[i] = vec4(vec3(x*2,0,z*2), ra);
-    }
+	sphere[i] = vec4(vec3(x*2,0.0,z*2), ra);
+    }*/
+
+    float ra = pow(2/float(NUMSPHEREES-1),4.0);
+    ra = 0.2 + 0.8*ra;
+    ra *= 1.0; //scale
+
+	vec3 position = vec3(-.5,0.0,-6);
+	sphere[0] = vec4(position, ra);
+    position = vec3(-3.0, 0.0, -3.0);
+    sphere[1] = vec4(position, ra);
+    position = vec3(2, 0, -3);
+    sphere[2] = vec4(position, ra);
 
     float le = 2.0;
     float px = 1.0*(2.0/resolution.y)*(1.0/le);
 
     vec3 ro, rd;
 
-    ro = cameraPos;
+    ro = cameraPos; 
 
     rd = getCam(ro) * normalize(vec3(uv, 1.0));
 
-	vec3 col = vec3(1);
+	vec3 col = vec3(0);
 
     float tmin = 1e20;
 
